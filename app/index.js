cc39bc0762d242c942323533433b40a8ca89f4a4
Martin Sandström <maartiin_91@hotmail.com>
2015-06-11 22:50:37 +0200

    Cleaning
    Made it a bit less T_T

--- app/index.js
+++ app/index.js
@@ -1,63 +1,55 @@
 $(function(){
 
-  var whiteboard = new Firebase('https://onlinewhiteboard.firebaseio.com/');
+    var whiteboard = new Firebase('https://onlinewhiteboard.firebaseio.com/');
+    var canvas = document.querySelector('#paint');
+    var ctx = canvas.getContext('2d');
+    var sketch = document.querySelector('#sketch');
+    var sketch_style = getComputedStyle(sketch);
+    var mouse = {x: 0, y: 0};
 
-  whiteboard.on('value', function(snapshot){
-    console.log(snapshot.val());
-    var img = new Image;
-    img.onload = function(){
-      ctx.drawImage(img,0,0); // Or at whatever offset you like
+    var onPaint = function() {
+        ctx.lineTo(mouse.x, mouse.y);
+        ctx.stroke();
+
+        whiteboard.child('context').set(canvas.toDataURL(), onComplete);
     };
-    var data = snapshot.val();
-    img.src = data.context;
-  });
 
+    var onComplete = function(error){
+        if(error)
+          console.log(error)
+    };
 
-  var canvas = document.querySelector('#paint');
-  var ctx = canvas.getContext('2d');
+    ctx.lineWidth = 5;
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = 'red';
 
-  var sketch = document.querySelector('#sketch');
-  var sketch_style = getComputedStyle(sketch);
-  canvas.width = parseInt(sketch_style.getPropertyValue('width'));
-  canvas.height = parseInt(sketch_style.getPropertyValue('height'));
+    canvas.width = parseInt(sketch_style.getPropertyValue('width'));
+    canvas.height = parseInt(sketch_style.getPropertyValue('height'));
 
-  var mouse = {x: 0, y: 0};
+    whiteboard.on('value', function(snapshot){
+        var img = new Image;
+        img.onload = function(){
+            ctx.drawImage(img,0,0);
+        };
+        var data = snapshot.val();
+        img.src = data.context;
+    });
 
-  /* Mouse Capturing Work */
-  canvas.addEventListener('mousemove', function(e) {
-    mouse.x = e.pageX - this.offsetLeft;
-    mouse.y = e.pageY - this.offsetTop;
-  }, false);
+    /* Mouse Capturing Work */
+    canvas.addEventListener('mousemove', function(e) {
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+    }, false);
 
+    canvas.addEventListener('mousedown', function(e) {
+        ctx.beginPath();
+        ctx.moveTo(mouse.x, mouse.y);
 
-    /* Drawing on Paint App */
-  ctx.lineWidth = 5;
-  ctx.lineJoin = 'round';
-  ctx.lineCap = 'round';
-  ctx.strokeStyle = 'red';
+        canvas.addEventListener('mousemove', onPaint, false);
+    }, false);
 
-  canvas.addEventListener('mousedown', function(e) {
-      ctx.beginPath();
-      ctx.moveTo(mouse.x, mouse.y);
-
-      canvas.addEventListener('mousemove', onPaint, false);
-  }, false);
-
-  canvas.addEventListener('mouseup', function() {
-      canvas.removeEventListener('mousemove', onPaint, false);
-  }, false);
-
-  var onPaint = function() {
-      ctx.lineTo(mouse.x, mouse.y);
-      ctx.stroke();
-
-      var data = canvas.toDataURL();
-      whiteboard.child('context').set(data, onComplete);
-  };
-
-  var onComplete = function(error){
-    if(error)
-      console.log(error)
-  };
-
+    canvas.addEventListener('mouseup', function() {
+        canvas.removeEventListener('mousemove', onPaint, false);
+    }, false);
 });
