c1ba13259800f76a9b923d3d844132be410e276d
Martin Sandström <maartiin_91@hotmail.com>
2015-06-11 23:18:37 +0200

    Update 2015-06-11T21:18:35.500Z

--- index.js
+++ index.js
@@ -36,85 +36,19 @@ $(function(){
         img.src = data.context;
     });
 
+    canvas.addEventListener('mousemove', function(e) {
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+    }, false);
 
-    var is_touch_device = ('ontouchstart' in document.documentElement) || ('createTouch' in document) || ('ontouchstart' in window);
+    canvas.addEventListener('mousedown', function(e) {
+        ctx.beginPath();
+        ctx.moveTo(mouse.x, mouse.y);
 
-     if (is_touch_device) {
-        // create a drawer which tracks touch movements
-        var drawer = {
-           isDrawing: false,
-           touchstart: function (coors) {
-              context.beginPath();
-              context.moveTo(coors.x, coors.y);
-              this.isDrawing = true;
-           },
-           touchmove: function (coors) {
-              if (this.isDrawing) {
-                 context.lineTo(coors.x, coors.y);
-                 context.stroke();
-              }
-           },
-           touchend: function (coors) {
-              if (this.isDrawing) {
-                 this.touchmove(coors);
-                 this.isDrawing = false;
-              }
-           }
-        };
+        canvas.addEventListener('mousemove', onPaint, false);
+    }, false);
 
-        // create a function to pass touch events and coordinates to drawer
-        function draw(event) {
-
-           // get the touch coordinates.  Using the first touch in case of multi-touch
-           var coors = {
-              x: event.targetTouches[0].pageX,
-              y: event.targetTouches[0].pageY
-           };
-
-           // Now we need to get the offset of the canvas location
-           var obj = sigCanvas;
-
-           if (obj.offsetParent) {
-              // Every time we find a new object, we add its offsetLeft and offsetTop to curleft and curtop.
-              do {
-                 coors.x -= obj.offsetLeft;
-                 coors.y -= obj.offsetTop;
-              }
-		  // The while loop can be "while (obj = obj.offsetParent)" only, which does return null
-		  // when null is passed back, but that creates a warning in some editors (i.e. VS2010).
-              while ((obj = obj.offsetParent) != null);
-           }
-
-           // pass the coordinates to the appropriate handler
-           drawer[event.type](coors);
-        }
-
-
-        // attach the touchstart, touchmove, touchend event listeners.
-        canvas.addEventListener('touchstart', draw, false);
-        canvas.addEventListener('touchmove', draw, false);
-        canvas.addEventListener('touchend', draw, false);
-
-        // prevent elastic scrolling
-        canvas.addEventListener('touchmove', function (event) {
-           event.preventDefault();
-        }, false);
-     }
-     else {
-       canvas.addEventListener('mousemove', function(e) {
-           mouse.x = e.pageX - this.offsetLeft;
-           mouse.y = e.pageY - this.offsetTop;
-       }, false);
-
-       canvas.addEventListener('mousedown', function(e) {
-           ctx.beginPath();
-           ctx.moveTo(mouse.x, mouse.y);
-
-           canvas.addEventListener('mousemove', onPaint, false);
-       }, false);
-
-       canvas.addEventListener('mouseup', function() {
-           canvas.removeEventListener('mousemove', onPaint, false);
-       }, false);
-     }
+    canvas.addEventListener('mouseup', function() {
+        canvas.removeEventListener('mousemove', onPaint, false);
+    }, false);
 });
